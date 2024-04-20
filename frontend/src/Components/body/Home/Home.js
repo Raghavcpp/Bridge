66dd335ddadae0cfd8b861dbcf4a625448fc559d
Manas <85065771+Manas15-coder@users.noreply.github.com>
2024-04-20 09:56:17 +0530

    Update Home.js

--- frontend/src/Components/body/Home/Home.js
+++ frontend/src/Components/body/Home/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
         <div className="landing-page">
             <div className="container">
                 <div className="info">
-                    <h1>Let's Make Project Review Easy 👨‍🏫 👨‍🎓</h1>
-                    <h3>Portal for Student & Professors <br></br></h3>
-                    <h6>Get started with your project submission and reviews in few clicks.</h6>
-                    <button className="btn btn-success"  >Welcome 👋</button>
+                <h1>Welcome to BridgeConnect Feedback Portal! 🌟</h1>
+<h6>Get answers within a few days, with our cutting-edge technology features! 💻🚀</h6>
+
+                    <button className="btn btn-success"  >Get Started Now</button>
                 </div>
                 <div className="image">
                     <img src={bg1} />
