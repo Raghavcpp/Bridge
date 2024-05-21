b441c4a9d0e9473f40b9e12247dfce42da65bb0b
Raghavcpp <raghav.cpp@gmail.com>
2024-05-21 20:22:02 +0530

    footer

--- frontend/src/Components/Footer/Footer.js
+++ frontend/src/Components/Footer/Footer.js
@@ -3,8 +3,7 @@ import "./footer.css";
 export default function Footer() {
     return (
         <div className="footer">
-            <div style={{ marginRight: '4px' }}><h6>© Review Portal 2022   </h6></div>
-            <div style={{ marginLeft: '4px' }} ><h6> BY ❤️ with <b> TAbhay & Pushkar Mourya </b></h6></div>
+            <div style={{ marginRight: '4px' }}><h6>© Communication Bridge 2024</h6></div>
         </ div>
     )
 }
