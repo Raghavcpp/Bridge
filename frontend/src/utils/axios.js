12b0a9d7225e1f9ddf15b1e45d23be29b1a8cda3
Raghav Verma <130084894+Raghavcpp@users.noreply.github.com>
2025-07-28 19:38:58 +0530

    api=axios

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 // src/utils/API.js
 import axios from "axios";
 
-const API = API.create({
+const API = axios.create({
   baseURL: process.env.apiurl,
   withCredentials: true, // if using cookies
 });
